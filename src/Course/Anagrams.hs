7f3be318d483e4b0ae3e8dfd9989abc4af99db97
iurii-kyrylenko <yuriproject@gmail.com>
2017-01-02 19:27:05 +0200

    Finished Anagrams

--- src/Course/Anagrams.hs
+++ src/Course/Anagrams.hs
@@ -28,17 +28,49 @@ Functions that might help
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-anagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
-anagrams =
-  error "todo: Course.Anagrams#anagrams"
+anagrams :: Chars -> Filename -> IO (List Chars)
+anagrams cs =
+  (<$>) (intersectBy equalIgnoringCase (permutations cs) . lines) . readFile
+
+-- anagrams cs path =
+--   (<$>) (intersectBy equalIgnoringCase (permutations cs) . lines) (readFile path)
+
+-- anagrams cs path =
+--   let prm = intersectBy equalIgnoringCase (permutations cs)
+--   in  (<$>) (prm . lines) (readFile path)
+
+-- * The law of composition
+--   `∀f g x.(f . g <$> x) ≅ (f <$> (g <$> x))`
+-- anagrams cs path =
+--   let prm = intersectBy equalIgnoringCase (permutations cs)
+--   in  prm . lines <$> readFile path
+
+-- anagrams cs path =
+--   let prm = intersectBy equalIgnoringCase (permutations cs)
+--   in  prm <$> lines <$> readFile path
+
+-- anagrams cs path = intersectBy equalIgnoringCase (permutations cs) <$> lines <$> readFile path
+-- anagrams cs path =
+--   let iod = lines <$> readFile path
+--       prm = intersectBy equalIgnoringCase (permutations cs)
+--   in  prm <$> iod
+
+-- import Course.Applicative
+-- import Course.Monad
+--
+-- anagrams cs path = do
+--   content <- readFile path
+--   return $ intersectBy equalIgnoringCase (permutations cs) (lines content)
 
 -- Compare two strings for equality, ignoring case
 equalIgnoringCase ::
   Chars
   -> Chars
   -> Bool
-equalIgnoringCase =
-  error "todo: Course.Anagrams#equalIgnoringCase"
+equalIgnoringCase = (==) `on` (toLower <$>)
+
+-- (\xs ys -> (toLower <$>) xs == (toLower <$>) ys) ≅ (==) `on` (toLower <$>)
+
+-- (\x y -> toLower x == toLower y) ≅ ((==) `on` toLower)
+
+-- equalIgnoringCase xs ys = all id $ zipWith (\x y -> toLower x == toLower y) xs ys
